fdaf16c7ab56a387ebfaa6dad36697ee7ed66d7e
Emma Gardner <emmagardner@outlook.com>
2015-01-25 13:47:57 -0500

    post object can be preloaded

--- emma-news/routes/index.js
+++ emma-news/routes/index.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
-// GET /posts :view all posts
+// GET /posts -view all posts
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
     if(err){ return next(err); }
@@ -15,7 +15,7 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-// POST /posts :create a post
+// POST /posts -create a post
 router.post('/posts', function(req, res, next) {
   var post = new Post(req.body);
 
@@ -25,6 +25,26 @@ router.post('/posts', function(req, res, next) {
     res.json(post);
   });
 });
+
+// pre-load post object
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("can't find post")); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+// GET /posts/:id -view a single post object
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+
+
 
 module.exports = router;
 
