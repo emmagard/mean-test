cecf6e18b85dc85ace29daab0a0b60d09c635706
Emma Gardner <emmagardner@outlook.com>
2015-01-25 11:08:27 -0500

    comments can be upvoted

--- app.js
+++ app.js
@@ -62,4 +62,16 @@ function($stateProvider, $urlRouterProvider) {
   'posts',
   function($scope, $stateParams, posts){
     $scope.post= posts.posts[$stateParams.id];
+    $scope.addComment= function(){
+      if($scope.body === ''){ return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author:'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+    $scope.incrementUpvotes= function(comment){
+      comment.upvotes += 1;
+    };
 }]);
